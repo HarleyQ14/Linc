a7304e4fddbe8cb43542fe880a6bfc4e11510a71
Kristen Dergousoff <harleyq14@gmail.com>
2012-10-04 13:34:53 -0600

    Reworked main menu layout to make use of tabs... not complete

--- src/com/syntacticsugar/linc/MainMenu.java
+++ src/com/syntacticsugar/linc/MainMenu.java
@@ -7,23 +7,36 @@ import com.syntacticsugar.linc.character.CharacterManager;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.app.TabActivity;
+import android.content.Intent;
+import android.content.res.Resources;
 import android.view.Menu;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TabHost;
 
-public class MainMenu extends Activity {
+public class MainMenu extends TabActivity {
 
 	private CharacterManager charManager = new CharacterManager(this);
 	
     @Override
     public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+        /*super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
 
         ArrayList<String> characters = charManager.getCharacterList();
         
         ListView listView = (ListView) findViewById(R.id.characterList);
-        listView.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, characters));
+        listView.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, characters));*/
+    	
+    	super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main_menu);
+ 
+		Resources ressources = getResources(); 
+		TabHost tabHost = getTabHost();
+ 
+		//set Windows tab as default (zero based)
+		tabHost.setCurrentTab(0);
     }
 
     @Override
