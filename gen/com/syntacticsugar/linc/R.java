1c3744dd288d0006a15737629cf9fb7eb6f07148
Kristen Dergousoff <harleyq14@gmail.com>
2012-10-05 12:29:41 -0600

    Tweaked the action bar at the top of the characters view.

--- gen/com/syntacticsugar/linc/R.java
+++ gen/com/syntacticsugar/linc/R.java
@@ -13,11 +13,13 @@ public final class R {
     public static final class drawable {
         public static final int ic_action_search=0x7f020000;
         public static final int ic_launcher=0x7f020001;
+        public static final int rounded_corners=0x7f020002;
     }
     public static final class id {
         public static final int add_char_bar=0x7f070001;
         public static final int add_char_button=0x7f070002;
-        public static final int menu_settings=0x7f070003;
+        public static final int editText1=0x7f070003;
+        public static final int menu_settings=0x7f070004;
         public static final int textView2=0x7f070000;
     }
     public static final class layout {
@@ -36,6 +38,7 @@ public final class R {
     }
     public static final class string {
         public static final int app_name=0x7f040000;
+        public static final int button_add_char=0x7f040008;
         public static final int hello_world=0x7f040001;
         public static final int menu_settings=0x7f040002;
         public static final int title_activity_abilities=0x7f040005;
